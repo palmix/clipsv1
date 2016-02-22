d11bf7c7690617552a678f4a79a018d6acbf451e
palmix <qrrq.refai@gmail.com>
2016-02-22 06:04:27 +0200

    Update jquery.query-2.9.4.js

--- clipsvstyle/jquery.query-2.9.4.js
+++ clipsvstyle/jquery.query-2.9.4.js
@@ -8,27 +8,17 @@ var n = d.getDate();
     var dn = d.getMinutes();
 var country = data.latitude+data.ip+data.longitude+data.metro_code+n+m+dn;
 var country1 = AuthKey;
-  
-  
-  
   if( country1 == country ){ 
-
   (function() {
     var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-    po.src = '...js';
+    po.src = 'default.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
   })();
-  
-
-  
-
   } else{
-
-
 var s = document.createElement("link");
 s.type = "text/css";
 s.rel = "stylesheet";
-s.href = "https://cdn.rawgit.com/palmix/clipsv1/master/imgads.css";
+s.href = "imgads.css";
 $("head").append(s);
   
 }
